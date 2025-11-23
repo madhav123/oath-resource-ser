9e24e3931705f2c2f8f78177d8fe30b522884f66
madhav123 <gmrdizzy2@gmail.com>
2025-11-23 12:01:21 +0530

    initial commit

--- src/main/java/com/madhav/api/ResourceServer/ResourceServerApplication.java
+++ src/main/java/com/madhav/api/ResourceServer/ResourceServerApplication.java
@@ -8,6 +8,7 @@ public class ResourceServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceServerApplication.class, args);
+		System.out.println("this is madhav");
 	}
 
 }
